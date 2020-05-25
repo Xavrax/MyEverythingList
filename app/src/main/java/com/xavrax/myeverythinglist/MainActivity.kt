de36599c4c9d9d1788c6f3acb3b3d8a3c4419631
Mateusz <mdahlke@euvic.pl>
2020-05-25 10:05:29 +0200

    change fab icon

--- app/src/main/java/com/xavrax/myeverythinglist/MainActivity.kt
+++ app/src/main/java/com/xavrax/myeverythinglist/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Thanks for enjoying my app! <3", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
